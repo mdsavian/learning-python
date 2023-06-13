63da2374e72716a6fab6ad11112f8e1430da5dd4
mdsavian <marlon.savian@gmail.com>
2023-06-13 11:13:39 -0300

    math operations and decimal division

---
+++ decimal_division.py
@@ -0,0 +1,11 @@
+# decimal by default
+print(5/2)
+
+# round to down 2
+print(5//2)
+
+# round to down so it will be -2
+print(-3//2)
+
+# using int to round to up -1
+print(int(-3/2))
---
+++ math.py
@@ -0,0 +1,6 @@
+import math
+
+print(math.floor(3/2))
+print(math.ceil(3/2))
+print(math.sqrt(2))
+print(math.pow(2, 3))
