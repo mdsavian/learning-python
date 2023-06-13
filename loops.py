9645abc8062747f97dfd1657194c4597d9ade822
mdsavian <marlon.savian@gmail.com>
2023-06-13 11:00:12 -0300

    loops

---
+++ loops.py
@@ -0,0 +1,19 @@
+# while
+
+n = 0
+while n >= 0 and n < 5:
+    n += 1
+    print(n)
+
+
+# for
+
+for i in range(5):
+    print(i)
+
+
+for i in range(2, 6):
+    print(i)
+
+for i in range(5, 1, -1):
+    print(i)
