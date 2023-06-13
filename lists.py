4ce75e63e0d3667ed113b6fae0c372b65f38ff88
mdsavian <marlon.savian@gmail.com>
2023-06-13 19:10:10 -0300

    list operations

--- lists.py
+++ lists.py
@@ -4,6 +4,7 @@ myList = []
 myList.append(1)
 myList.append(2)
 myList.append(3)
+myList.append(4)
 
 print(myList[0])
 print(myList[1])
@@ -29,3 +30,41 @@ for s in strings:
 for name in names:
     print(name)
 
+# slicing array
+print(myList[2:4])
+
+# unpacking
+a, b, c, d = myList
+print(a, b, c, d)
+
+# getting the index and the position value using enumerate
+for i, n in enumerate(myList):
+    print(i, n)
+
+# interate through two arrays
+for n, name in zip(numbers, names):
+    print(n, name)
+
+# reversing array
+numbers.reverse()
+print(numbers)
+
+# sorting
+numbers.sort()
+print(numbers)
+
+# sortint reverse
+numbers.sort(reverse=True)
+print(numbers)
+
+# with lambda function
+names.sort(key=lambda name: len(name))
+print(names)
+
+# comprehension
+newNames = [x for x in names if "e" in x]
+print(newNames)
+
+# create 2d arrays
+arr = [[0] * 4 for i in range(4)]
+print(arr)
